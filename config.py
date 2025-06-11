1b1b5ba6ff0b7b6093f1ea5dcc8a2cc5e81160ef
unknown <cliffowino22@gmail.com>
2025-06-11 13:23:04 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+WIDTH, HEIGHT = 1920, 1080
+CAR_SIZE = (60, 60)
+BORDER_COLOR = (255, 255, 255, 255)
+RADAR_ANGLES = [-90, -60, -30, 0, 30, 60, 90]
+ASSETS = {
+    "CAR": "core/assets/car.png",
+    "MAP": "core/assets/map2.png"
+}
---
+++ main.py
@@ -0,0 +1,22 @@
+import neat
+from simulation import run_simulation
+
+
+if __name__ == "__main__":
+    
+    # Load Config
+    config_path = "Assets/config.txt"
+    config = neat.config.Config(neat.DefaultGenome,
+                                neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet,
+                                neat.DefaultStagnation,
+                                config_path)
+
+    # Create Population And Add Reporters
+    population = neat.Population(config)
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+    
+    # Run Simulation For A Maximum of 1000 Generations
+    population.run(run_simulation, 1000)
